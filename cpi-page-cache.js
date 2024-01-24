0fd09ba15b74b0f32be52c4b58807f235f2b3849
Michael Wood <michaelwood33311@icloud.com>
2024-01-24 08:58:42 -0500

    added database connectivity

--- cpi-page-cache.js
+++ cpi-page-cache.js
@@ -1,4 +1,22 @@
+require('dotenv').config();
 const axios = require('axios');
+const mysql = require('mysql2/promise');
+
+const { CPI_PRODUCTION_HOST, CPI_PRODUCTION_DATABASE, CPI_PRODUCTION_USER, CPI_PRODUCTION_PASSWORD } = process.env;
+
+const sql = mysql.createPool({
+    host: CPI_PRODUCTION_HOST,
+    user: CPI_PRODUCTION_USER,
+    database: CPI_PRODUCTION_DATABASE,
+    password: CPI_PRODUCTION_PASSWORD,
+    waitForConnections: true,
+    connectionLimit: 10,
+    maxIdle: 10, // max idle connections, the default value is the same as `connectionLimit`
+    idleTimeout: 60000, // idle connections timeout, in milliseconds, the default value 60000
+    queueLimit: 0,
+    enableKeepAlive: true,
+    keepAliveInitialDelay: 0,
+  });
 
 const getCategories = async host => {
     const categories = [];
@@ -22,7 +40,8 @@ const getCategories = async host => {
 
 const program = async () => {
     const categories = await getCategories('www.competitionpolicyinternational.com');
-    console.log(categories);
+    const databases = await sql.query(`SHOW DATABASES`)
+    
 }
 
 const programWrapper = () => {
