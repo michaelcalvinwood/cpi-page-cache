26b4f6fd9168b62ee48ba92a5d2fe8bb17cb0b1d
Michael Wood <michaelwood33311@icloud.com>
2024-01-24 08:48:43 -0500

    added fetching all category ids and slugs

--- cpi-page-cache.js
+++ cpi-page-cache.js
@@ -1,12 +1,28 @@
 const axios = require('axios');
 
 const getCategories = async host => {
-    const response = await axios.get(`https://${host}/wp-json/wp/v2/categories`);
-    console.log(response.data);
+    const categories = [];
+    let page = 1;
+    const perPage = 100;
+    let count = 100;
+    while (count > 0) {
+        const response = await axios.get(`https://${host}/wp-json/wp/v2/categories/?page=${page}&per_page=100`);
+        count = response.data.length;
+        for (let i = 0; i < count; ++i) {
+            const category = response.data[i];
+            const { id, slug } = category;
+            categories.push({id, slug});
+        }
+        console.log(page, count);
+        ++page;
+    }
+    return categories;
+    
 }
 
 const program = async () => {
     const categories = await getCategories('www.competitionpolicyinternational.com');
+    console.log(categories);
 }
 
 const programWrapper = () => {
